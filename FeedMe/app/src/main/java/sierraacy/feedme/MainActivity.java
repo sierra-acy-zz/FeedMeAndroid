ebe643bf3f2b302fd13235a036f9c08deebf19f6
Sierra Acy <sierra.acy@gmail.com>
2016-10-25 19:49:23 -0500

    main layout complete. replacing advanced settings drawer with button for now

--- FeedMe/app/src/main/java/sierraacy/feedme/MainActivity.java
+++ FeedMe/app/src/main/java/sierraacy/feedme/MainActivity.java
@@ -2,6 +2,10 @@ package sierraacy.feedme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.btn_settings) {
+            //open settings
+            Toast.makeText(this, "Settings!", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
